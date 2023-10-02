30237df618ed3e2364b08d4f69cf00242e545801
Mohammadhossein <siahkamarii1363@gmail.com>
2023-10-02 02:23:13 +0200

    master

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import SideBar from './components/sideBar/sideBar';
+import 'tailwindcss/tailwind.css';
+import './Styles.css'
+
+
+function App() {
+  return (<div>
+    <SideBar />
+  </div>
+  );
+}
+
+export default App;
