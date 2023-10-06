66239db44a0d861072c40c35d1d9a23878e1585e
Mohammadhossein <siahkamarii1363@gmail.com>
2023-10-06 23:49:13 +0200

    master

---
+++ src/components/headMain/headMain.jsx
@@ -0,0 +1,11 @@
+import pexels01 from './../../assets/pexels01.jpg'
+
+
+const HeadMain = () => {
+    return (
+        <div>
+            <img className='w-6 ' src={pexels01} alt="" />
+        </div>);
+}
+
+export default HeadMain;
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,19 +1,10 @@
-
+import HeadMain from "../headMain/headMain";
 
 const Main = () => {
     return (
-        <div className="  h-[1000px]">
-            <div className="felx flex-col  h-[1000px] bg-red-300">
-                <div className="bg-slate-50 w-64 h-[48px] border-b-2 ">
-                    <div className="w-10 h-10">img</div>
-                    <div className="flex flex-col">
-                        <div className="w-5 h-5 bg-slate-400"></div>
-                        <div className="w-5 h-5 bg-slate-300"></div>
-                    </div>
-                    <div>...</div>
-                </div>
-                <div className="w-96 h-[585px] bg-blue-500"></div>
-            </div>
+        <div className="w-full h-96 bg-black">
+            <HeadMain />
+           
         </div>
     );
 }
