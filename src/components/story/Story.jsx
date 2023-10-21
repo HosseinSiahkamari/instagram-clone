76ac6fbc302987df48a9c308e2b86f29d4b6bafe
Mohammadhossein <siahkamarii1363@gmail.com>
2023-10-21 21:41:55 +0200

    master

--- src/components/story/Story.jsx
+++ src/components/story/Story.jsx
@@ -5,7 +5,6 @@ const Story = ({ avatar, username }) => {
         <div className="flex flex-col justify-center items-center px-2 w-20 h-20">
             <img className="rounded-full  overflow-x-scroll w-14 h-14 " src={avatar} alt="avatar" />
             <p className="truncate w-14">{username} </p>
-            <h1>kjbkbkkjbnbnkbh</h1>
         </div>
         );
 }
