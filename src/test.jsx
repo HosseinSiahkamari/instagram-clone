3f95f13988cbce53fe3cd544a3bb3f794d53b6cd
Mohammadhossein <siahkamarii1363@gmail.com>
2023-10-31 22:19:51 +0100

    master

--- src/test.jsx
+++ src/test.jsx
@@ -1,9 +1,27 @@
+import React, { useState } from 'react';
 
+const DropdownMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-const test = () => {
-    return ( <div>
-        console.log('testing');
-    </div> );
-}
- 
-export default test;
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="dropdown">
+      <button onClick={toggleMenu} className="dropdown-toggle">
+        Menu
+      </button>
+
+      {isOpen && (
+        <ul className="dropdown-menu">
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default DropdownMenu;
